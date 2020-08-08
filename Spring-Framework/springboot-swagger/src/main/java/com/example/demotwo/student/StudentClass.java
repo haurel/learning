18d59717b40a61947562cc4b6a22c0a70e31e199
haurel <h.aurel97@yahoo.com>
2020-08-08 11:20:07 +0300

    Adding ApiParam, ApiModel, ApiModelProperty

--- Spring-Framework/springboot-swagger/src/main/java/com/example/demotwo/student/StudentClass.java
+++ Spring-Framework/springboot-swagger/src/main/java/com/example/demotwo/student/StudentClass.java
@@ -1,5 +1,7 @@
 package com.example.demotwo.student;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,9 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel(description = "Student class")
 public class StudentClass {
+    @ApiModelProperty(value = "Student first name", example = "Vatsal")
     private String firstName;
+    @ApiModelProperty(value = "Student last name", example = "Riotal")
     private String lastName;
+    @ApiModelProperty(value = "Student phone number", example = "0612233122")
     private String phone;
+    @ApiModelProperty(value = "Student's age", example = "23")
     private Integer age;
 }
--- Spring-Framework/springboot-swagger/src/main/java/com/example/demotwo/student/StudentController.java
+++ Spring-Framework/springboot-swagger/src/main/java/com/example/demotwo/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.demotwo.student;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
 public class StudentController {
     private List<StudentClass> studentList = new ArrayList<>();
 
@@ -26,10 +26,10 @@ public class StudentController {
     @PostMapping("/student")
     @ApiOperation(value = "Add new student", response = StudentClass.class)
     public StudentClass addStudent(
-            @RequestParam String firstName,
-            @RequestParam String lastName,
-            @RequestParam String phone,
-            @RequestParam Integer age
+          @ApiParam(value = "Write first name", required = true) @RequestParam String firstName,
+          @ApiParam(value = "Write last name", required = true) @RequestParam String lastName,
+          @ApiParam(value = "Write phone number", required = true) @RequestParam String phone,
+          @ApiParam(value = "Write age", required = true) @RequestParam Integer age
     ){
         StudentClass newStudent = StudentClass.builder()
                 .firstName(firstName)
