f9dd49883d2005e9bf9959a09675a356958004b1
haurel <h.aurel97@yahoo.com>
2020-08-13 18:02:46 +0300

    exception demo

---
+++ Spring-Framework/spring-validation/src/main/java/com/validationdemo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.validationdemo.repository;
+
+import com.validationdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
