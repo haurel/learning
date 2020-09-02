136b672ee85e8b1c7e97e33893a65fe233667db5
haurel <h.aurel97@yahoo.com>
2020-09-02 17:21:15 +0300

    edit&delete post

--- laravel/blog-laravel/resources/views/posts/show.blade.php
+++ laravel/blog-laravel/resources/views/posts/show.blade.php
@@ -13,4 +13,11 @@
     </div>
     <hr>
     <small>Writte on {{ $post->created_at }}</small>
+    <hr>
+    <a href="/posts/{{ $post->id }}/edit" class="btn btn-secondary">Edit</a>
+
+    {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-sm-right']) !!}
+        {{ Form::hidden('_method', 'DELETE') }}
+        {{ Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!! Form::close() !!}
 @endsection
